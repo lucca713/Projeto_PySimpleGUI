5c0f597b7ca9f9245a4c5958353eaa986d1d4387
lucca713 <luccacerasomma03@gmail.com>
2023-05-31 22:00:04 -0300

    Botão cancelar funcionando, correção de alguns bugs(ainda falta alguns) e consulta no banco de dados realizado com sucesso

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
+def fazer_login(usuario, senha):
+    link_bd = 'https://cadastro-user-dc0d0-default-rtdb.firebaseio.com/'
+    url = f"{link_bd}/Login.json"
+
+    requisicao = requests.get(url)
+    dados = requisicao.json()
+
+    for chave, valor in dados.items():
+        if valor["cliente"] == usuario and valor["password"] == senha:
+            return True
+
+    return False
+
+
 import PySimpleGUI as sg
 import requests
 import json
@@ -11,25 +25,33 @@ layout = [
 [sg.Text('', key='mensagem')],
 ]
 
-
+#evento tipo o baotao que vc clica (a ação) o valor é aquilo que ele retora ou faz
 
 janela = sg.Window('Login', layout)
 
 
 while True:
 
-    event, values = janela.read()
-    if event == sg.WIN_CLOSED:
-        break
-    elif event == 'login':
+    event, values = janela.read() #eventos e valores da janela principal (layout)
+    
+    if event == 'login': #para verificar se o login existe no Banco de dados
         print('ok')
-        
+        usuario = values['usuario']
+        senha = values['senha']
+
+        if fazer_login(usuario, senha):
+            sg.popup('Login bem-sucedido')
+        else:
+            sg.popup('Usuário ou senha inválidos')
        
         
-       #------------------janela login ---------------------         
 
-    if event == 'New_login':
-        event = janela.close() #fecha a janela anterior quando entrar para criar nova conta
+
+
+       #------------------janela criar a conta ---------------------         
+
+    elif event == 'New_login':
+        janela.hide() #Esconde a janela anterior quando entrar para criar nova conta
 
         Criar_conta = [
 
@@ -56,9 +78,28 @@ while True:
                 senha = {'password': new_values['nova_senha']}
 
                 login = {'cliente': new_values['novo_usuario'],
+                         
                          'password': new_values['nova_senha']
-                         }    
+                         } 
+                nova_janela.close() #fecha a janela nova janela que é a do cadastro usuario
+                janela.un_hide() #abre a janela de login
             
+#------- funcionalidade do botão cancelar -----------
+
+            elif new_event == 'Cancelar' or new_event == sg.WINDOW_CLOSED:
+                nova_janela.close() #fecha a janela nova janela que é a do cadastro usuario
+                janela.un_hide() #abre a janela de login
+        
+        
+                    
+    if event == sg.WINDOW_CLOSED:#ele fecha a janela principal
+        break           
+
+
+
+
+
+
 
 
 
@@ -69,7 +110,8 @@ link_bd  = 'https://cadastro-user-dc0d0-default-rtdb.firebaseio.com/'
 requisicao = requests.post(f'{link_bd}/Nome.json', data= json.dumps(user))
 requisicao = requests.post(f'{link_bd}/Senha.json', data= json.dumps(senha))
 requisicao = requests.post(f'{link_bd}/Login.json', data= json.dumps(login))
-                    
+requisicao = requests.get(f'{link_bd}.json')
+
                            
 
 if requisicao.status_code == 200:
@@ -78,12 +120,11 @@ else:
     print('Falha ao enviar os dados para o Firebase.')
 
     """
-    Criei a tela de criar usuario,
+    falta: quando o usuario fazer o login levar ele para uma tela feita em django por mim.
+    
+    fazer um botaão esqueci a senha 
 
-    fiz o codigo para mandar o novo usuario para o banco de dados
+    fazer ele virar um executavel
 
-    falta fazer a parte do login funcionar com a senha e usuario certo de acordo com oq esta no banco de dados,
-
-    fazer um campo de esqueci a senha.
-
+    corrigir bugs
     """
