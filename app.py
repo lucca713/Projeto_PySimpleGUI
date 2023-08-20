9567ece763820ef5db2db6cca28b8814ebead5a2
lucca713 <luccacerasomma03@gmail.com>
2023-08-20 03:16:34 -0300

    botão 'esqueci a senha' feito e funcoes separadas para um codigo mais limpo pagina django em andamento

--- app.py
+++ app.py
@@ -1,41 +1,8 @@
 import  webbrowser
 import PySimpleGUI as sg
-import requests
-import json
+from funcoes import fazer_login, Banco_dado, recuperar_senha  
 
-def fazer_login(usuario, senha):
-    link_bd = 'https://caduser-40a03-default-rtdb.firebaseio.com/' # link do bd
-    url = f"{link_bd}/Login.json" #transforma as informações do banco em um dicionario json
 
-    requisicao = requests.get(url) #ele da um get (pega) as informações do banco de dados 
-    dados = requisicao.json() # tranforma as informações em uma arquivo json
-
-    for chave, valor in dados.items(): # ele começa um loop lendo chaves e valores do banco de dados
-        if valor["cliente"] == usuario and valor["password"] == senha:
-            #aqui se o valor da chave cliente for igual oque o usuario digitou e a mesma coisa para senha ele retorna true
-            return True
-
-    return False
-def Banco_dado():
-
-    link_bd  = 'https://caduser-40a03-default-rtdb.firebaseio.com/'
-    global requisicao
-    requisicao = requests.post(f'{link_bd}/Nome.json', data= json.dumps(user))
-    requisicao = requests.post(f'{link_bd}/Senha.json', data= json.dumps(senha))
-    requisicao = requests.post(f'{link_bd}/Login.json', data= json.dumps(login))
-
-def recuperar_senha(usuario1):
-    link_bd = 'https://caduser-40a03-default-rtdb.firebaseio.com/' # link do bd
-    url = f"{link_bd}/Login.json" #transforma as informações do banco em um dicionario json
-
-    requisicao = requests.get(url) #ele da um get (pega) as informações do banco de dados 
-    dados = requisicao.json() # tranforma as informações em uma arquivo json
-
-    for chave, valor in dados.items(): # ele começa um loop lendo chaves e valores do banco de dados
-        if valor["usuario_esqueci_senha"] == usuario1: #usa só nome para pegar a senha
-           senha1 = valor['password'] 
-           sg.popup(senha1)
-           
 
 
 layout = [  
@@ -136,8 +103,7 @@ while True:
                 janela.un_hide()
             if event_newPassword == 'nova_senha_botao':
 
-                usuario1 = values['usuario_esqueci_senha']
-
+                usuario1 = values_newPassword['usuario_esqueci_senha']
                 recuperar_senha(usuario1)
                 
 
